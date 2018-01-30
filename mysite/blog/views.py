5d1226553e3c67ddcf69e720a03f64abdf03ce1c
shikharvaish28 <shikarvaish@gmail.com>
2018-01-30 18:04:16 +0530

    list view made for blog

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,10 +6,11 @@ from django.shortcuts import render
 # Create your views here.
 from .models import PostModel
 
+def post_model_detail_view(request):
+    return render(request , template_path, context)
 
 def post_model_list_view(request):
     qs = PostModel.objects.all()
-    print (qs)
     template_path="blog/list_view.html"
     context = {
     "object_list": qs
