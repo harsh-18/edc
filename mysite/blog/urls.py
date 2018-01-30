d17a02d97392e830d396220662ec84b3f241430a
shikharvaish28 <shikarvaish@gmail.com>
2018-01-30 18:59:59 +0530

    detail view for single page blog is working now

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$', views.post_model_list_view, name='post_list'),
+    url(r'^$', views.post_model_list_view, name='list'),
+    url(r'^(?P<id>\d+)/$', views.post_model_detail_view, name='detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,12 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
 from .models import PostModel
 
-def post_model_detail_view(request):
-    return render(request , template_path, context)
+def post_model_detail_view(request, id=None):
+    obj = PostModel.objects.get(id=id)
+    template="blog/detail_view.html"
+    context = {
+    "object": obj,
+    }
+    return render(request , template, context)
 
 def post_model_list_view(request):
     qs = PostModel.objects.all()
@@ -16,3 +22,10 @@ def post_model_list_view(request):
     "object_list": qs
     }
     return render(request , template_path , context)
+
+
+def recent_post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # sorted the posts according to published_date
+    return render(request, 'blog/post_list.html', {'posts': posts})
+    # render the blog posts with the page request
